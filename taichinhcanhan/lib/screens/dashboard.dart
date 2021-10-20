e3e5a917ad7f3fc9c6769cddd0012a7a33e6decc
Pham Quynh Anh <pqa.dev@gmail.com>
2021-10-21 05:07:32 +0900

    Use VScode quick fix to extract to new widget

--- taichinhcanhan/lib/screens/dashboard.dart
+++ taichinhcanhan/lib/screens/dashboard.dart
@@ -9,24 +9,35 @@ class DashboardScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text('Your Financial Assistant')),
         drawer: const MenuDrawer(),
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (int index) {
-            switch (index) {
-              case 0:
-                Navigator.pushNamed(context, '/');
-                break;
-              case 1:
-                Navigator.pushNamed(context, '/expense');
-                break;
-            }
-          },
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard), label: 'Dashboard'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.money_off), label: 'Expense'),
-          ],
-        ),
+        bottomNavigationBar: MenuBottom(),
         body: const Text('Dashboard'));
+  }
+}
+
+class MenuBottom extends StatelessWidget {
+  const MenuBottom({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: (int index) {
+        switch (index) {
+          case 0:
+            Navigator.pushNamed(context, '/');
+            break;
+          case 1:
+            Navigator.pushNamed(context, '/expense');
+            break;
+        }
+      },
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard), label: 'Dashboard'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.money_off), label: 'Expense'),
+      ],
+    );
   }
 }
